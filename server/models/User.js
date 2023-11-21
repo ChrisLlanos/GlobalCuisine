26b0f103db90800e8c6511ad4817fe75dad9e438
evillarreal18 <ericvillarreal81997@gmail.com>
2023-11-20 19:07:10 -0600

    added email and password to user.js file

--- server/models/User.js
+++ server/models/User.js
@@ -13,7 +13,7 @@ const userSchema = new Schema(
             required:true,
             unique:true
         },
-        password: {
+        password:{
             type:String,
             required:true
         },
