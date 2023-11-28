f27086524474afd0fa15725d32b4098bb5b32afe
evillarreal18 <ericvillarreal81997@gmail.com>
2023-11-27 21:28:10 -0600

    added outline for home page and added some code in main.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,6 +6,21 @@ import DetailedMenu from '../components/DetailedMenu';
 import { QUERY_MENU } from '../utils/queries';
 
 const Home = () => {
+    return (
+        <div>
+          <h2>Welcome to Our Restaurant</h2>
+          <p>Delicious food just a click away!</p>
+          
+          <div>
+            <Link to="/sign-up">
+              <button>Sign Up</button>
+            </Link>
+            <Link to="/sign-in">
+              <button>Sign In</button>
+            </Link>
+          </div>
+        </div>
+      );
+    };
 
-}
-
+    export default Home;
